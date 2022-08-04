fa6e47696711df894a2e3ffbf0edcbb21c9b0e33
Sean Moriarty <spmoriarty1@gmail.com>
2022-08-03 17:34:57 -0700

    created join table, completely lost

--- sql/setup.sql
+++ sql/setup.sql
@@ -2,9 +2,10 @@
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP table if exists authors;
 DROP table if exists books;
+DROP table if exists books_authors;
 
 CREATE table authors (
-    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR NOT NULL,
     dob INT,
     pob VARCHAR
@@ -12,9 +13,17 @@ CREATE table authors (
 
 
 CREATE table books (
-    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     title VARCHAR NOT NULL,
     released INT NOT NULL
+);
+
+CREATE table books_authors (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    book_id BIGINT,
+    author_id BIGINT,
+    FOREIGN KEY (book_id) REFERENCES books(id),
+    FOREIGN KEY (author_id) REFERENCES authors(id)
 );
 
 
@@ -31,3 +40,10 @@ INSERT INTO books (title, released) VALUES
 ('1984', 1949),
 ('The Great Gatsby', 1925),
 ('Amerika', 1927);
+
+
+INSERT INTO books_authors (book_id, author_id) VALUES
+(1,1),
+(2,2),
+(3,3),
+(4,4);
